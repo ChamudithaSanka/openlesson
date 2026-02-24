a6d60851e4456c37f35c77ebb0c2cc0236ecad51
Isanka02 <isankasonali7990@gmail.com>
2026-02-24 22:39:36 +0530

    updated the codes with propper comments

--- backend/src/routes/studentRegRoute.js
+++ backend/src/routes/studentRegRoute.js
@@ -6,11 +6,21 @@ import {
   deactivateStudent
 } from '../controllers/studentRegController.js';
 
+import { body, validationResult } from 'express-validator';
+
+
 const router = express.Router();
 
+//register students
 router.post('/register', registerStudent);
+
+//student login
 router.post('/login', loginStudent);
+
+//update profile details
 router.put('/profile/:id', updateProfile);
+
+//deactivate the account of the student
 router.put('/deactivate/:id', deactivateStudent);
 
 export default router;
