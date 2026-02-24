9f8da184b49f1ce33069248de68e29f3da9672fd
Isanka02 <isankasonali7990@gmail.com>
2026-02-24 23:31:58 +0530

    added notification logic in create feedback after creating a feedback

--- backend/src/controllers/feedbackController.js
+++ backend/src/controllers/feedbackController.js
@@ -9,6 +9,16 @@ export const createFeedback = async (req, res) => {
     const feedback = new Feedback({ studentId, teacherId, rating, comment });
     await feedback.save();
 
+
+      //notification for feedback submitted
+    await Notification.create({
+    studentId: feedback.studentId,
+    type: "Feedback",
+    message: `Your feedback for the teacher has been submitted successfully.`,
+    });
+
+
+    
     res.status(201).json({ message: "Feedback created successfully", feedback });
   } catch (err) {
     res.status(500).json({ message: "Server error", error: err.message });
