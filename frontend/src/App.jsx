9ea856f4b581d1e5697de27ea10423efa83a19fa
Isanka02 <isankasonali7990@gmail.com>
2026-02-25 22:35:44 +0530

    feat(frontend): setup React Router routes for all pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,29 @@
 import React from 'react';
+import { Route } from 'react-router';
 
 function App() {
   return (
     <div>
-      App
+      
+
+      <Routes>
+        <Route path= "/" element = {<RegisterPage/>}/>
+        <Route path= "/login" element = {<LoginPage/>}/> 
+        <Route path= "/profile" element = {<ProfilePage/>}/> 
+
+
+
+        <Route path= "/feedbackDetails" element = {<feedbackDetails/>}/>
+        <Route path= "/feedbackList" element = {<feedbackList/>}/>
+        <Route path= "/feedbackForm" element = {<feedbackForm/>}/>
+
+        <Route path= "/myReports" element = {<myReports/>}/>
+        <Route path= "/repoertDetails" element = {<reportDetails/>}/>
+        <Route path= "/reportForm" element = {<reportForm/>}/>
+
+        <Route path= "/notifications" element = {<myNotifications/>}/>
+
+      </Routes>
     </div>
   );
 }
