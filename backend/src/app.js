34e68ec999b8571e1947f0bcd6d6898c62cd31bd
Adeepa Wedage <IT23826304@my.sliit.lk>
2026-03-01 16:13:53 +0530

    update app.js

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,6 +2,8 @@ import express from "express";
 import cors from "cors";
 import studySessionRoutes from "./routes/studySession.route.js";
 import announcementRoutes from "./routes/announcement.route.js";
+import gradeRoutes from "./routes/grade.route.js";
+import subjectRoutes from "./routes/subject.route.js";
 
 const app = express();
 
@@ -13,6 +15,8 @@ app.use(express.urlencoded({ extended: true }));
 // Routes
 app.use("/api/study-sessions", studySessionRoutes);
 app.use("/api/announcements", announcementRoutes);
+app.use("/api/grades", gradeRoutes);
+app.use("/api/subjects", subjectRoutes);
 
 // Error handler
 app.use((err, req, res, next) => {
