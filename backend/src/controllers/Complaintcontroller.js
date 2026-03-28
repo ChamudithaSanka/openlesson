e9c42bcd51eeda96e136b6947a0bafb1a027da4b
Chamuditha Sanka <chamudithasanka98@gmail.com>
2026-03-28 14:01:30 +0530

    fixed the destruering issue in complaintController

--- backend/src/controllers/Complaintcontroller.js
+++ backend/src/controllers/Complaintcontroller.js
@@ -4,7 +4,7 @@ import Student from "../models/studentRegModel.js";
 // 🔹 Create Complaint
 export const createComplaint = async (req, res) => {
   try {
-    const { studentId,subject, description, category } = req.body;
+    const { subject, description, category } = req.body;
 
     if (!subject || !description) {
       return res.status(400).json({
